c8c81e6aab96df073c29f56a933b40dd4c3b8c2b
Roman <doroshroman9644@gmail.com>
2026-04-01 22:29:01 +1000

    refactor: replace JDBC connection with DataSource

--- src/main/java/org/roman/carsharing/dao/car/CarJDBC.java
+++ src/main/java/org/roman/carsharing/dao/car/CarJDBC.java
@@ -1,8 +1,10 @@
 package org.roman.carsharing.dao.car;
 
 import org.roman.carsharing.model.Car;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,24 +12,11 @@ import java.util.List;
 @Component
 public class CarJDBC implements CarDataBase {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/Carsharing";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private DataSource dataSource;
 
-    private static Connection connection;
-
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    @Autowired
+    public CarJDBC(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     // Возвращает список всех машин из таблицы
@@ -36,23 +25,22 @@ public class CarJDBC implements CarDataBase {
 
         List<Car> carList = new ArrayList<>();
 
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("SELECT car_id, year_of_release, model, equipment FROM Car");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT car_id, year_of_release, model, equipment FROM Car")) {
 
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    Car car = new Car();
 
-            while (resultSet.next()) {
-                Car car = new Car();
+                    car.setCar_id(resultSet.getInt("car_id"));
+                    car.setYear_of_release(resultSet.getInt("year_of_release"));
+                    car.setModel(resultSet.getString("model"));
+                    car.setEquipment(resultSet.getString("equipment"));
 
-                car.setCar_id(resultSet.getInt("car_id"));
-                car.setYear_of_release(resultSet.getInt("year_of_release"));
-                car.setModel(resultSet.getString("model"));
-                car.setEquipment(resultSet.getString("equipment"));
-
-                carList.add(car);
+                    carList.add(car);
+                }
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -66,23 +54,23 @@ public class CarJDBC implements CarDataBase {
 
         Car car = null;
 
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("SELECT car_id, year_of_release, model, equipment FROM car WHERE car_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT car_id, year_of_release, model, equipment FROM car WHERE car_id = ?")) {
 
             preparedStatement.setInt(1, id);
 
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            resultSet.next();
+                resultSet.next();
 
-            car = new Car();
+                car = new Car();
 
-            car.setCar_id(resultSet.getInt("car_id"));
-            car.setYear_of_release(resultSet.getInt("year_of_release"));
-            car.setModel(resultSet.getString("model"));
-            car.setEquipment(resultSet.getString("equipment"));
-
+                car.setCar_id(resultSet.getInt("car_id"));
+                car.setYear_of_release(resultSet.getInt("year_of_release"));
+                car.setModel(resultSet.getString("model"));
+                car.setEquipment(resultSet.getString("equipment"));
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -93,9 +81,9 @@ public class CarJDBC implements CarDataBase {
     // Добавляет новую машину в таблицу
     @Override
     public void addObject(Car car) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("INSERT INTO Car (year_of_release, model , equipment) VALUES (?, ?, ?)");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("INSERT INTO Car (year_of_release, model , equipment) VALUES (?, ?, ?)")) {
 
             preparedStatement.setInt(1, car.getYear_of_release());
             preparedStatement.setString(2, car.getModel());
@@ -111,9 +99,9 @@ public class CarJDBC implements CarDataBase {
     // Обновляет конкретную машину
     @Override
     public void updateObject(Car car, int id) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("UPDATE Car SET year_of_release = ?, model = ?, equipment = ? WHERE car_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("UPDATE Car SET year_of_release = ?, model = ?, equipment = ? WHERE car_id = ?")) {
 
             preparedStatement.setInt(1, car.getYear_of_release());
             preparedStatement.setString(2, car.getModel());
@@ -131,9 +119,9 @@ public class CarJDBC implements CarDataBase {
     // Удаляет машину
     @Override
     public void deleteObject(int id) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("DELETE FROM Car WHERE car_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("DELETE FROM Car WHERE car_id = ?")) {
 
             preparedStatement.setInt(1, id);
 
@@ -151,24 +139,24 @@ public class CarJDBC implements CarDataBase {
 
         List<Car> carList = new ArrayList<>();
 
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("SELECT * FROM Car WHERE person_id IS NULL");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT * FROM Car WHERE person_id IS NULL")) {
 
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            while (resultSet.next()) {
-                Car car = new Car();
+                while (resultSet.next()) {
+                    Car car = new Car();
 
-                car.setCar_id(resultSet.getInt("car_id"));
-                car.setPerson_id(resultSet.getInt("person_id"));
-                car.setYear_of_release(resultSet.getInt("year_of_release"));
-                car.setModel(resultSet.getString("model"));
-                car.setEquipment(resultSet.getString("equipment"));
+                    car.setCar_id(resultSet.getInt("car_id"));
+                    car.setPerson_id(resultSet.getInt("person_id"));
+                    car.setYear_of_release(resultSet.getInt("year_of_release"));
+                    car.setModel(resultSet.getString("model"));
+                    car.setEquipment(resultSet.getString("equipment"));
 
-                carList.add(car);
+                    carList.add(car);
+                }
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -183,26 +171,26 @@ public class CarJDBC implements CarDataBase {
 
         List<Car> carList = new ArrayList<>();
 
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("SELECT * FROM Car WHERE person_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT * FROM Car WHERE person_id = ?")) {
 
             preparedStatement.setInt(1, id);
 
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            while (resultSet.next()) {
-                Car car = new Car();
+                while (resultSet.next()) {
+                    Car car = new Car();
 
-                car.setCar_id(resultSet.getInt("car_id"));
-                car.setPerson_id(resultSet.getInt("person_id"));
-                car.setYear_of_release(resultSet.getInt("year_of_release"));
-                car.setModel(resultSet.getString("model"));
-                car.setEquipment(resultSet.getString("equipment"));
+                    car.setCar_id(resultSet.getInt("car_id"));
+                    car.setPerson_id(resultSet.getInt("person_id"));
+                    car.setYear_of_release(resultSet.getInt("year_of_release"));
+                    car.setModel(resultSet.getString("model"));
+                    car.setEquipment(resultSet.getString("equipment"));
 
-                carList.add(car);
+                    carList.add(car);
+                }
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -213,9 +201,9 @@ public class CarJDBC implements CarDataBase {
     // Купить машину
     @Override
     public void buyObject(int person_id, int car_id) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("UPDATE Car SET person_id = ? WHERE car_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("UPDATE Car SET person_id = ? WHERE car_id = ?")) {
 
             preparedStatement.setInt(1, person_id);
             preparedStatement.setInt(2, car_id);
@@ -230,9 +218,9 @@ public class CarJDBC implements CarDataBase {
     // Продать машину
     @Override
     public void sellObject(int id) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("UPDATE Car SET person_id = null WHERE car_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("UPDATE Car SET person_id = null WHERE car_id = ?")) {
 
             preparedStatement.setInt(1, id);
 
--- src/main/java/org/roman/carsharing/dao/person/PersonJDBC.java
+++ src/main/java/org/roman/carsharing/dao/person/PersonJDBC.java
@@ -1,8 +1,10 @@
 package org.roman.carsharing.dao.person;
 
 import org.roman.carsharing.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,24 +12,11 @@ import java.util.List;
 @Component
 public class PersonJDBC implements PersonDataBase {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/Carsharing";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private DataSource dataSource;
 
-    private static final Connection connection;
-
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    @Autowired
+    public PersonJDBC(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     // Возвращает список людей из таблицы
@@ -36,23 +25,22 @@ public class PersonJDBC implements PersonDataBase {
 
         List<Person> personList = new ArrayList<>();
 
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("SELECT * FROM Person");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT * FROM Person")) {
 
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    Person person = new Person();
 
-            while (resultSet.next()) {
-                Person person = new Person();
+                    person.setPerson_id(resultSet.getInt("person_id"));
+                    person.setAge(resultSet.getInt("age"));
+                    person.setName(resultSet.getString("name"));
+                    person.setSurname(resultSet.getString("surname"));
 
-                person.setPerson_id(resultSet.getInt("person_id"));
-                person.setAge(resultSet.getInt("age"));
-                person.setName(resultSet.getString("name"));
-                person.setSurname(resultSet.getString("surname"));
-
-                personList.add(person);
+                    personList.add(person);
+                }
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -66,22 +54,23 @@ public class PersonJDBC implements PersonDataBase {
 
         Person person = null;
 
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("SELECT * FROM Person WHERE person_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("SELECT * FROM Person WHERE person_id = ?")) {
 
             preparedStatement.setInt(1, id);
 
-            ResultSet resultSet = preparedStatement.executeQuery();
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
-            resultSet.next();
+                resultSet.next();
 
-            person = new Person();
+                person = new Person();
 
-            person.setPerson_id(resultSet.getInt("person_id"));
-            person.setAge(resultSet.getInt("age"));
-            person.setName(resultSet.getString("name"));
-            person.setSurname(resultSet.getString("surname"));
+                person.setPerson_id(resultSet.getInt("person_id"));
+                person.setAge(resultSet.getInt("age"));
+                person.setName(resultSet.getString("name"));
+                person.setSurname(resultSet.getString("surname"));
+            }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -93,8 +82,9 @@ public class PersonJDBC implements PersonDataBase {
     // Добавляет нового человека в таблицу
     @Override
     public void addObject(Person person) {
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Person (age, name, surname) VALUES (?, ?, ?)");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("INSERT INTO Person (age, name, surname) VALUES (?, ?, ?)")) {
 
             preparedStatement.setInt(1, person.getAge());
             preparedStatement.setString(2, person.getName());
@@ -110,9 +100,9 @@ public class PersonJDBC implements PersonDataBase {
     // Обновляет данные о человеке
     @Override
     public void updateObject(Person person, int id) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("UPDATE Person SET age = ?, name = ?, surname = ? WHERE person_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("UPDATE Person SET age = ?, name = ?, surname = ? WHERE person_id = ?")) {
 
             preparedStatement.setInt(1, person.getAge());
             preparedStatement.setString(2, person.getName());
@@ -130,9 +120,9 @@ public class PersonJDBC implements PersonDataBase {
     // Удаляет человека из таблицы
     @Override
     public void deleteObject(int id) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("DELETE FROM Person WHERE person_id = ?");
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("DELETE FROM Person WHERE person_id = ?")) {
 
             preparedStatement.setInt(1, id);
 
